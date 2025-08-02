de8c669d79cff1ffde2e28c6aa1e588aa8201769
mann-makhecha <mannmakhecha1506@gmail.com>
2025-08-02 20:43:56 +0530

    Add Bootstrap integration and theme variables for HRMS layout

--- components/layout/sidebar.php
+++ components/layout/sidebar.php
@@ -1,54 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-</head>
-<body>
-     <div class="container-fluid">
-        <div class="row">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark min-vh-100">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white">
-                    <div class="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto">
-                        <img src="../assets/img/logo.png" height="60" width="60">
-                      
-                    </div>
-                    
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start w-100" id="menu">
-                        <li class="nav-item w-100">
-                            <a href="hr_dashboard" class="nav-link text-white px-0">
-                                <i class="fs-4 bi bi-speedometer2"></i>
-                                <span class="ms-2 d-none d-sm-inline">Dashboard</span>
-                            </a>
-                        </li>
-                        <li class="nav-item w-100">
-                            <a href="employees.php" class="nav-link text-white px-0">
-                                <i class="fs-4 bi bi-people"></i>
-                                <span class="ms-2 d-none d-sm-inline">Employees</span>
-                            </a>
-                        </li>
-                        <li class="nav-item w-100">
-                            <a href="attendance.php" class="nav-link text-white px-0">
-                                <i class="fs-4 bi bi-calendar3"></i>
-                                <span class="ms-2 d-none d-sm-inline">Attendance</span>
-                            </a>
-                        </li>
-                        <li class="nav-item w-100">
-                            <a href="leave_management.php" class="nav-link text-white px-0">
-                                <i class="fs-4 bi bi-card-list"></i>
-                                <span class="ms-2 d-none d-sm-inline">Leave Management</span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col py-3">
-            </div>
-        </div>
+<?php include 'header.php'; ?>
+<div class="d-flex">
+    <!-- Sidebar -->
+    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 250px; min-height: 100vh;">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none">
+        <span class="fs-4 text-primary">HRMS</span>
+      </a>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+          <a href="#" class="nav-link active bg-primary text-white" aria-current="page">
+            Dashboard
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link text-primary">
+            Employees
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link text-primary">
+            Attendance
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link text-primary">
+            Payroll
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link text-primary">
+            Settings
+          </a>
+        </li>
+      </ul>
     </div>
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-</body>
-</html>
+
+    <!-- Main content -->
+    <div class="p-4" style="flex: 1;">
+      <h2 class="text-primary">Welcome to HRMS</h2>
+      <p>This sidebar is built using Bootstrap only with your custom color variables.</p>
+    </div>
+  </div>
+    <?php include 'footer.php'; ?>
